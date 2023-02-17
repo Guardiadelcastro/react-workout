82dd4fcc7edce27ef4c381c7af50d6c2ce0e01a4
Alejandro Castro de la Guardia <guardiadelcastro@gmail.com>
2023-02-17 13:37:51 +0000

    feat: faq

---
+++ src/faq.jsx
@@ -0,0 +1,260 @@
+export function Faq() {
+  return (
+    <div className="">
+      <div className="">
+        <h2 className="">FAQ</h2>
+        <p className="">Frequenty asked questions</p>
+      </div>
+      <div className="">
+        <div className="">
+          <details className="">
+            <summary className="">
+              <span> What is a SAAS platform?</span>
+              <span className="">
+                <svg
+                  fill="none"
+                  height="24"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path d="M6 9l6 6 6-6"></path>
+                </svg>
+              </span>
+            </summary>
+            <p className="">
+              SAAS platform is a cloud-based software service that allows users
+              to access and use a variety of tools and functionality.
+            </p>
+          </details>
+        </div>
+        <div className="">
+          <details className="">
+            <summary className="">
+              <span> How does billing work?</span>
+              <span className="">
+                <svg
+                  fill="none"
+                  height="24"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path d="M6 9l6 6 6-6"></path>
+                </svg>
+              </span>
+            </summary>
+            <p className="">
+              We offers a variety of billing options, including monthly and
+              annual subscription plans, as well as pay-as-you-go pricing for
+              certain services. Payment is typically made through a credit card
+              or other secure online payment method.
+            </p>
+          </details>
+        </div>
+        <div className="">
+          <details className="">
+            <summary className="">
+              <span> Can I get a refund for my subscription?</span>
+              <span className="">
+                <svg
+                  fill="none"
+                  height="24"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path d="M6 9l6 6 6-6"></path>
+                </svg>
+              </span>
+            </summary>
+            <p className="">
+              We offers a 30-day money-back guarantee for most of its
+              subscription plans. If you are not satisfied with your
+              subscription within the first 30 days, you can request a full
+              refund. Refunds for subscriptions that have been active for longer
+              than 30 days may be considered on a case-by-case basis.
+            </p>
+          </details>
+        </div>
+        <div className="">
+          <details className="">
+            <summary className="">
+              <span> How do I cancel my subscription?</span>
+              <span className="">
+                <svg
+                  fill="none"
+                  height="24"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path d="M6 9l6 6 6-6"></path>
+                </svg>
+              </span>
+            </summary>
+            <p className="">
+              To cancel your We subscription, you can log in to your account and
+              navigate to the subscription management page. From there, you
+              should be able to cancel your subscription and stop future
+              billing.
+            </p>
+          </details>
+        </div>
+        <div className="">
+          <details className="">
+            <summary className="">
+              <span> Can I try this platform for free?</span>
+              <span className="">
+                <svg
+                  fill="none"
+                  height="24"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path d="M6 9l6 6 6-6"></path>
+                </svg>
+              </span>
+            </summary>
+            <p className="">
+              We offers a free trial of its platform for a limited time. During
+              the trial period, you will have access to a limited set of
+              features and functionality, but you will not be charged.
+            </p>
+          </details>
+        </div>
+        <div className="">
+          <details className="">
+            <summary className="">
+              <span> How do I access documentation?</span>
+              <span className="">
+                <svg
+                  fill="none"
+                  height="24"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path d="M6 9l6 6 6-6"></path>
+                </svg>
+              </span>
+            </summary>
+            <p className="">
+              Documentation is available on the company's website and can be
+              accessed by logging in to your account. The documentation provides
+              detailed information on how to use the , as well as code examples
+              and other resources.
+            </p>
+          </details>
+        </div>
+        <div className="">
+          <details className="">
+            <summary className="">
+              <span> How do I contact support?</span>
+              <span className="">
+                <svg
+                  fill="none"
+                  height="24"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path d="M6 9l6 6 6-6"></path>
+                </svg>
+              </span>
+            </summary>
+            <p className="">
+              If you need help with the platform or have any other questions,
+              you can contact the company's support team by submitting a support
+              request through the website or by emailing support@We.com.
+            </p>
+          </details>
+        </div>
+        <div className="">
+          <details className="">
+            <summary className="">
+              <span> Do you offer any discounts or promotions?</span>
+              <span className="">
+                <svg
+                  fill="none"
+                  height="24"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path d="M6 9l6 6 6-6"></path>
+                </svg>
+              </span>
+            </summary>
+            <p className="">
+              We may offer discounts or promotions from time to time. To stay
+              up-to-date on the latest deals and special offers, you can sign up
+              for the company's newsletter or follow it on social media.
+            </p>
+          </details>
+        </div>
+        <div className="">
+          <details className="">
+            <summary className="">
+              <span> How do we compare to other similar services?</span>
+              <span className="">
+                <svg
+                  fill="none"
+                  height="24"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path d="M6 9l6 6 6-6"></path>
+                </svg>
+              </span>
+            </summary>
+            <p className="">
+              This platform is a highly reliable and feature-rich service that
+              offers a wide range of tools and functionality. It is
+              competitively priced and offers a variety of billing options to
+              suit different needs and budgets.
+            </p>
+          </details>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-
+import { Faq } from "./faq";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/faq",
+    element: <Faq />,
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
